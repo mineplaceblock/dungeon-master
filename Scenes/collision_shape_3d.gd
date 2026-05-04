f220e126c57e3f431da80b413b8e14d606e1fe74
mineplaceblock <mineplaceblock@gmail.com>
2026-05-04 13:21:44 +0200

    purificaçao

--- Scenes/collision_shape_3d.gd
+++
@@ -1,3 +0,0 @@
-# Selecciona el GridMap y en el inspector pon esto en un script
-func _ready():
-	$GridMap.Altura 0()
--- salto.gd
+++
@@ -1,4 +0,0 @@
-extends Node3D  # o CharacterBody2D, Control, etc.
-
-func _ready():
-	$AnimationPlayer.play("mixamo_com")
