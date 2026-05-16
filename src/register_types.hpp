b1b84adf16255ffef37d2c70eb08494f18b55cfb
mineplaceblock <mineplaceblock@gmail.com>
2026-05-16 17:57:29 +0200

    No se que es asi que se va

--- src/register_types.hpp
+++
@@ -1,6 +0,0 @@
-#include <godot_cpp/core/class_db.hpp>
-
-using namespace godot;
-
-void initialize_module(ModuleInitializationLevel p_level);
-void uninitialize_module(ModuleInitializationLevel p_level);
